bbc8ed6485865e05c4dae67e9e9a83dbb6ded938
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2023-08-16 22:47:33 +0700

    Config: install ShadCN components

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 function TopBar() {
   return (
@@ -26,6 +27,7 @@ function TopBar() {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: { organizationSwitcherTrigger: "py-2 px-4" },
           }}
         />
